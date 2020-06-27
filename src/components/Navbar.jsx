b5b2f28269e8ae0887833c8a4afa0b2b8280b7bf
IAmShawn98 <shawnluther99@gmail.com>
2020-06-27 14:29:01 -0400

    Installed reactstrap for better use of buttons and components, started media queries for mobile design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'; // allows for use of components
+import React, { Component } from 'react'; // allows for use of components
 import logo from '../logo.png'; // Logo displayed next to company name on navbar
 import { Link } from "react-router-dom"; // Enables routing from page to page
 
@@ -8,45 +8,48 @@ import { Link } from "react-router-dom"; // Enables routing from page to page
 
 
 
- class  Navbar extends Component {
-   render(){
+class Navbar extends Component {
+  render() {
 
-   
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <Link className="navbar-brand text-white text-uppercase ml-5" id="nav-banner" to="/">Green Thumb Guys<img src ={logo} alt ="logo" style = {{width: "50px"}}/></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        {/* Green Thumb Guys Logo */}
+        <Link className="navbar-brand text-white text-uppercase ml-5" id="nav-banner" to="/">
+          Green Thumb Guys <img id="GreenGuy" src={logo} alt="logo" style={{ width: "50px" }} />
+        </Link> <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav ml-auto">
-          
+
             <li className="nav-item active">
               <Link className="nav-link text-white text-uppercase mx-5" to="/" ><i class="fas fa-home"></i>&nbsp;Home <span class="sr-only">(current)</span></Link>
             </li>
-            
-           
+
+
             <li className="nav-item">
               <Link className="nav-link text-white text-uppercase mx-5" to="/about"><i class="fas fa-info-circle"></i>&nbsp;About Us</Link>
             </li>
-            
-           
+
+
             <li className="nav-item">
               <Link className="nav-link text-white text-uppercase mx-5" to="/services"><i class="fas fa-tasks"></i>&nbsp;Services</Link>
             </li>
-            
-            
+
+
             <li className="nav-item">
               <Link className="nav-link text-white text-uppercase mx-5" to="/login"><i class="fas fa-user-plus"></i>&nbsp;Join / Login</Link>
             </li>
-            
+
           </ul>
-          
+
         </div>
       </nav>
-    
+
     )
-   }
+  }
 }
 
 
