333efa2cd4d7dd72c4d12553bc7f50215113e3f6
IAmShawn98 <shawnluther99@gmail.com>
2020-07-03 15:24:34 -0400

    Reworked the media queries to be on seperate documents for accessibility and readability, improved overall responsiveness on both home page and services.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'; // Allows for use of components
 import { Button, ButtonGroup } from 'reactstrap';
 import { Link } from "react-router-dom";
+import StockSlider from './StockSlider';
 
 // A P P _ _ C O M P O N E N T _ _ F O R _ _ H O M E  _ _ P A G E
 
@@ -9,34 +10,29 @@ class Home extends Component {
     render() {
         return (
             <div className="home-section" id="home">
-                <div id="LawnImageSlides" className="carousel slide carousel-fade" data-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-caption">
-                            <h1 id="ThumbTitle">Professional Landscaping</h1>
-                            <p id="ThumbContext" className="display-4 mb-5">We provide professional residential &amp; commercial landscaping services.</p>
-                            <div className="ServiceLinks">
-                                <ButtonGroup id="desktopThumb">
-                                    <Link to="/about"><Button color="primary" className="display-1" id="btnCompany">View Our Services</Button></Link>
-                                    <Link to="/login"><Button color="success" id="btnClient">Become A Client</Button></Link>
-                                </ButtonGroup>
-
-                                <ButtonGroup vertical id="mobileThumb">
-                                    <Link to="/about"><Button color="primary" className="display-1" id="btnCompany">View Our Services</Button></Link>
-                                    <Link to="/login"><Button color="success" id="btnClient">Become A Client</Button></Link>
-                                </ButtonGroup>
-                            </div>
-                        </div>
-                        <div className="carousel-item active">
-                            <img className="d-block w-100" src="./images/backgrounds/lawn.jpg" alt="lawn.jpg" />
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="lawn.jpg" />
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://images.pexels.com/photos/321542/pexels-photo-321542.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="lawn.jpg" />
-                        </div>
+                {/* Home Page Context */}
+                <div className="carousel-caption">
+                    {/* Home Page Brand */}
+                    <h1 id="ThumbTitle">Professional Landscaping</h1>
+                    {/* Professional Description */}
+                    <p id="ThumbContext" className="display-4 mb-5">We provide professional residential &amp; commercial landscaping services.</p>
+                    {/* Service Link Buttons (Desktop/Mobile) */}
+                    <div className="ServiceLinks">
+                        {/* Desktop Service Links  */}
+                        <ButtonGroup id="desktopThumb">
+                            <Link to="/about"><Button color="primary" className="display-1" id="btnCompany">View Our Services</Button></Link>
+                            <Link to="/login"><Button color="success" id="btnClient">Become A Client</Button></Link>
+                        </ButtonGroup>
+                        {/* Mobile Service Links */}
+                        <ButtonGroup vertical id="mobileThumb">
+                            <Link to="/about"><Button color="primary" className="display-1" id="btnCompany">View Our Services</Button></Link>
+                            <Link to="/login"><Button color="success" id="btnClient">Become A Client</Button></Link>
+                        </ButtonGroup>
                     </div>
                 </div>
+
+                {/*  Home Carousel Stock Slider */}
+                <StockSlider />
             </div>
         );
     };
